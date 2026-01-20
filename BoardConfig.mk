Bangkk-Sources/android_device_motorola_bangkk

fogos: Switch back to our own CLO kernel source
Change-Id: Ie4dc8891dd04248db230abea8e78c920b3fbd19b

BoardConfig.mk
@@ -21,7 +21,7 @@ ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/sku/manifest_n.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=bangkk
-TARGET_KERNEL_CONFIG += vendor/ext_config/moto-holi-bangkk.config
+TARGET_KERNEL_CONFIG += vendor/bangkk_defconfig
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))

AyakaUI-Devices/android_device_motorola_fogos

BoardConfig.mk
@@ -21,7 +21,7 @@ ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/sku/manifest_n.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=fogos
-TARGET_KERNEL_CONFIG += vendor/ext_config/moto-holi-fogos.config
+TARGET_KERNEL_CONFIG += vendor/fogos_defconfig
 
 # Kernel Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))