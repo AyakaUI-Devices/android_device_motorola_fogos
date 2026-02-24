98323abedb7cceeffdc914ce5a02e5af0dfbe043
Barry <barryofc11@gmail.com>
2026-02-24 20:53:48 -0300

    fogos: Bringup PixelOS
    Signed-off-by: Barry <barryofc11@gmail.com>

--- custom_fogos.mk
+++ custom_fogos.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/motorola/fogos/device.mk)
 # Inherit some common PixelOS stuff.
 $(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := flavor_fogos
+PRODUCT_NAME := custom_fogos
 PRODUCT_DEVICE := fogos
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
